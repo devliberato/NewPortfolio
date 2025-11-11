d8830f3e9f1094977a213932dc46931fb2dc99e4
Matheus999814 <liberatomatheus52@gmail.com>
2025-11-11 18:25:09 -0300

    consertando responsividade navbar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-let menuIcon = document.querySelector("menu-icon");
+let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menuIcon.addEventListener("click", () => {
-
-    menuIcon.classList.toggle("bx-x")
-    navbar.classList.toggle("active")
-
-})
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+});
