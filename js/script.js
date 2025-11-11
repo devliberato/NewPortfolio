3dfccbe1d2ea99bb4cc7ac0c6e11853985296740
Matheus999814 <liberatomatheus52@gmail.com>
2025-11-11 18:11:40 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let menuIcon = document.querySelector("menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+
+    menuIcon.classList.toggle("bx-x")
+    navbar.classList.toggle("active")
+
+})
