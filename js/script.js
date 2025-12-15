fe028e4b782bd89814023be3abe89bbd6239ce45
Matheus999814 <liberatomatheus52@gmail.com>
2025-12-15 15:40:01 -0300

    Alterações no portfolio

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("bx-x");
+
   navbar.classList.toggle("active");
 });
